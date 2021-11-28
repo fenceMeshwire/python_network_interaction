7522d68bdf8b2aff4546a2ba57252b20dd03ea06
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2021-11-28 18:47:25 +0100

    Create uploadSSH.py

---
+++ uploadSSH.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# uploadSSH.py
+
+# Purpose: Upload a file via SSH to remote server.
+
+import os
+import paramiko
+
+def upload(localDirectory, remoteDirectory):
+    serverIP = ''
+    usrname = ''
+    passwd = ''
+    ssh = paramiko.SSHClient()
+    ssh.load_host_keys(os.path.expanduser(os.path.join('~', '.ssh', 'known_hosts')))
+    ssh.connect(serverIP, username=usrname, password=passwd)
+    sftp = ssh.open_sftp()
+    sftp.put(localDirectory, remoteDirectory)
+    sftp.close()
+    ssh.close()
+
+filename = ''
+localDirectory = '' + filename
+remoteDirectory = '' + filename
+
+try:
+    upload(localDirectory, remoteDirectory)
+except BaseException as err:
+    print('Please check the name of the directory and the file.', err)
