39c7d6b4817a90ac00517b2e08df6c7e3c9dc3e3
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-21 08:47:12 +0200

    Create 04_patch_area_designation_small_company.py
    Create patch designations for a patch panel in a small company.

---
+++ 04_patch_area_designation_small_company.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 04_patch_area_designation_small_company.py
+
+LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', \
+    'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+LETTERS_DIGITS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', \
+    'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', \
+    1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def create_designations(LETTERS, LETTERS_DIGITS):
+    result = []
+    for first in LETTERS:
+        designation = str(first)
+        for second in LETTERS_DIGITS:           
+            designation += str(second)
+            result.append(designation)
+            designation = str(first)
+    return result
+
+if __name__ == '__main__':
+    result = create_designations(LETTERS, LETTERS_DIGITS)
+    print(result)
+    # ['AA', 'AB', 'AC', ..., 'A7', 'A8', 'A9', 'BA', 'BB', ...]
+    len(result)
+    # 792
